2962a25789dc10c424b350c2270bce80bca198e8
Yasindu20 <yasindudemel@gmail.com>
2025-08-05 13:46:05 +0530

    Tailwind Errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   darkMode: 'class',
   theme: {
@@ -9,6 +10,10 @@ module.exports = {
       colors: {
         primary: {
           50: '#eff6ff',
+          100: '#dbeafe',
+          200: '#bfdbfe',
+          300: '#93c5fd',
+          400: '#60a5fa',
           500: '#3b82f6',
           600: '#2563eb',
           700: '#1d4ed8',
